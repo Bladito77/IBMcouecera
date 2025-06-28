cdfee1a5a587c006cc9100e1562ef54ae37f4002
bladito77 <edblatoco@gmail.com>
2025-06-27 20:48:10 -0500

    Actualización de funciones y estilos

--- employeeDetails.js
+++ employeeDetails.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization : 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 , specialization : 'Python'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 , specialization : 'Java' },
     //... Se pueden agregar más registros de empleados aquí
 ];
 
@@ -29,4 +29,10 @@ function findEmployeeById(employeeId) {
     else{
       document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
      }
+ }
+
+ function findSpecialization () {
+    const EmployeesEspcialJAvasc = employees.filter(employee => employee.specialization === 'Javascript');
+     const EmployeesEspcialJAvascDisplay = EmployeesEspcialJAvasc.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - ${employee.specialization}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = EmployeesEspcialJAvascDisplay;
  }
